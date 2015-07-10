acfde844c785505fb17456eb6ab32dfbd91a09a8
mitsuru <hayasaka.mitsuru@gmail.com>
2015-07-10 18:39:24 +0900

    sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :omniauthable,
-         :rememberable, :trackable, :validatable
+         :registerable, :rememberable, :trackable, :validatable, :omniauth_providers => [:timecrowd]
+
+  def self.find_for_oauth(auth)
+    user = self.find_by(uid: auth.uid, provider: auth.provider)
+    unless user
+      user = self.create(
+        uid: auth.uid,
+        provider: auth.provider,
+        email: self.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
+  end
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@timeout.dev"
+  end
 end
