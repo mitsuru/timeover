db3983bfba3d9021c2ecdc466094ea6f59ebd83a
mitsuru <hayasaka.mitsuru@gmail.com>
2015-07-14 10:59:00 +0900

    enhance sign in page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :omniauthable,
-         :registerable, :rememberable, :trackable, :validatable, :omniauth_providers => [:timecrowd]
+         :rememberable, :trackable, :validatable, :omniauth_providers => [:timecrowd]
 
   has_many :milestones
   has_many :time_entries
